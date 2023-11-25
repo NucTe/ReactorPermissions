6611c2eb8b162bbf12c7d03f01aa395d8822608f
1mag1n33 <kopeccarson@gmail.com>
2023-11-24 22:32:28 -0500

    Got the Local Site up

--- src/main/java/me/nuclearteam/reactorpermissions/ReactorPermissions.java
+++ src/main/java/me/nuclearteam/reactorpermissions/ReactorPermissions.java
@@ -1,23 +1,43 @@
 package me.nuclearteam.reactorpermissions;
 
+import me.nuclearteam.nuclearlib.Nuclearlib;
 import me.nuclearteam.nuclearlib.bukkit.MongoDb.ConfigManager;
 import me.nuclearteam.nuclearlib.bukkit.MongoDb.MongoDbManager;
+import me.nuclearteam.reactorpermissions.Website.Site;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.eclipse.jetty.server.Server;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+
 
 public final class ReactorPermissions extends JavaPlugin {
     private ConfigManager configManager;
     private MongoDbManager mongoDbManager;
 
+    Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+
+
     @Override
     public void onEnable() {
+        // Mongodb
+
+        mongoLogger.setLevel(Level.SEVERE);
         configManager = new ConfigManager(this);
         mongoDbManager = new MongoDbManager(this, "ReactorPermissions");
         String connectionString = configManager.getMongoDBConnectionString();
         mongoDbManager.connect(connectionString);
-        
 
+        // Jetty
 
-
+        int port = 8080; // Use the port you want
+        Server server = new Server(port);
+        try {
+            Site.web(server);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- src/main/java/me/nuclearteam/reactorpermissions/Website/Site.java
+++ src/main/java/me/nuclearteam/reactorpermissions/Website/Site.java
@@ -1,4 +1,28 @@
 package me.nuclearteam.reactorpermissions.Website;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
 public class Site {
+    public static void web(Server server) throws Exception {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
+
+        // Set the base resource (adjust the path as needed)
+        String resourceBase = Site.class.getClassLoader().getResource("webapp").toExternalForm();
+        resourceHandler.setResourceBase(resourceBase);
+
+        // Create a handler list and add the ResourceHandler
+        HandlerList handlers = new HandlerList();
+        handlers.addHandler(resourceHandler);
+
+        // Set the handlers for the server
+        server.setHandler(handlers);
+
+        // Start the server
+        server.start();
+        server.join();
+    }
 }
